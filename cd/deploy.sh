97e53e59d51423b285647c25d347919675947bdb
Andre de Fontana Ignacio <andre.ignacio@netshoes.com>
2017-07-26 14:26:03 -0300

    Coveralls configuration.

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn clean deploy coveralls:report -P sign,build-extras --settings cd/mvnsettings.xml -DrepoToken=$COVERALLS_REPO_TOKEN
+    mvn clean deploy coveralls:report -DrepoToken=$COVERALLS_REPO_TOKEN -P sign,build-extras --settings cd/mvnsettings.xml
 fi
